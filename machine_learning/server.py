00945b2f9f605576ef742ff9b278e1cf4fded1bf
robillardstudio <87386484+robillardstudio@users.noreply.github.com>
2022-04-25 16:30:54 +0200

    Update server.py

--- machine_learning/server.py
+++ machine_learning/server.py
@@ -1,12 +1,9 @@
 '''
 CCM
-
 Copyright (c) 2021 Gaëtan Robillard and Jolan Goulin
-
 BSD Simplified License.
 For information on usage and redistribution, and for a DISCLAIMER OF ALL
 WARRANTIES, see the file, "LICENSE.txt," in this distribution.
-
 '''
 
 # PI ZERO SERVER
